56e5740a8d037c784421774f618bff55f207b436
julian1501 <juliangootzen@live.nl>
2025-03-05 20:51:00 +0100

    Modify observer definition procedure
    Use LMI

--- classes/MO.m
+++ classes/MO.m
@@ -8,17 +8,22 @@ classdef MO
         numOutputs % the number of sensors sensing the system
         numObservers % number of observers in this multi observer
         numOutputsObserver % number of outputs that each observer has
+        eigenvalues % eigenvalues of the observer
         CSet % Output matrix of each observer i
         CSetIndices % Matrix containing the origin of each Ci
+        A % Matrix containing (possibly different) A matrices for each observer
+        LSet % Matrix containing the L matrices for each observer
+        KSet % Matrix containing the K matrices for each observer
     end
 
     methods
-        function obj = MO(sys,attack)
+        function obj = MO(sys,attack,numOutputsObserver)
             %UNTITLED2 Construct an instance of this class
             %   Detailed explanation goes here
             obj.sys = sys;
             obj.attack = attack;
             obj.numOutputs = obj.sys.nx;
+            obj.A = obj.sys.A;
 
             if 2*obj.attack.numAttacks > obj.numOutputs
                 warning(["More than half of the sensors are attacked",...
@@ -26,11 +31,15 @@ classdef MO
                         obj.attack.numAttacks,obj.numOutputs);
             end
             
-            obj.numOutputsObserver  = obj.numOutputs - obj.attack.numAttacks;
+            obj.numOutputsObserver  = numOutputsObserver;
             obj.numObservers        = nchoosek(obj.numOutputs,obj.numOutputsObserver);
             
-            [obj.CSet,obj.CSetIndices] = CSetSetup(obj);
+            [obj.CSet,obj.CSetIndices] = obj.CSetSetup();
 
+            obj.eigenvalues = -1:-1:-obj.numOutputs;
+            [obj.LSet, obj.KSet] = obj.defineObservers(true);
+
+            
         end
 
         function [CSet,CSetIndices] = CSetSetup(obj)
@@ -41,6 +50,85 @@ classdef MO
                          obj.numObservers);
             for i = 1:obj.numObservers
                 CSet(:,:,i) = obj.sys.C(CSetIndices(i,:),:);
+            end
+        end
+
+        function [LSet, KSet] = defineObservers(obj,diagnosticCheck)
+            % This function defines the observers based on the LMI as in
+            % section 5 of (Chong, 2025, Observer design for nonlinear 
+            % systems with asynchronously sampled and maliciously corrupted
+            % measurements)
+            LSet = zeros(obj.sys.nx,obj.numOutputsObserver,obj.numObservers);
+            KSet = zeros(obj.sys.nx,obj.numOutputsObserver,obj.numObservers);
+            ebar = 1; % max slope nonlinearity
+            ebarM = diag(repmat(ebar^-1,1,obj.numOutputs));
+
+            for i = 1:1:obj.numObservers
+                % Generate observer construction matrix (OCM) that has to
+                % satisfy LMI: OCM =< 0
+                Ci = obj.CSet(:,:,i);
+                setlmis([])
+                L = lmivar(2,[obj.sys.nx obj.numOutputsObserver]);
+                K = lmivar(2,[obj.sys.nx obj.numOutputsObserver]);
+
+                Rs = lmivar(1,[obj.sys.nx 1]);
+                Nu = lmivar(1,[1 0]);
+                Ds = lmivar(1,repmat([1 0],obj.numOutputs,1));
+                Mua = lmivar(1,[1 0]);
+                Mud = lmivar(1,[1 0]);
+
+                MuaLMI = newlmi; % 0 < Mua
+                lmiterm([-MuaLMI 1 1 Mua],eye(obj.sys.nx),1);
+
+                MudLMI = newlmi; % 0 < Mud
+                lmiterm([-MudLMI 1 1 Mud],eye(obj.sys.nx),1);
+
+                DsLMI = newlmi; % 0 < Ds
+                lmiterm([-DsLMI 1 1 Ds],1,1);
+
+                RsLMI = newlmi; % 0 < Rs
+                lmiterm([-RsLMI 1 1 Rs],1,1);
+                
+                NuLMI = newlmi; % 0 < Mu
+                lmiterm([-NuLMI 1 1 Nu],eye(obj.sys.nx),1);
+                
+                LKLMI = newlmi; % Whole big thing
+                lmiterm([LKLMI 1 1 Rs],1,obj.A,'s');
+                lmiterm([LKLMI 1 1 L],Rs,Ci,'s');
+                lmiterm([LKLMI 1 1 Nu],1,1);
+
+                lmiterm([LKLMI 1 2 Rs],obj.sys.B,1);
+                lmiterm([LKLMI 1 2 -Ds],obj.sys.C',1);
+                lmiterm([LKLMI 1 2 -K],Ci',Ds');
+
+                lmiterm([LKLMI 1 3 Rs],-1,1);
+                lmiterm([LKLMI 1 4 Rs],1,1);
+
+                lmiterm([LKLMI 2 2 Ds],-2,ebarM);
+                lmiterm([LKLMI 3 3 Mua],-1,1);
+                lmiterm([LKLMI 4 4 Mud],-1,1);
+                
+                % construct lmi and solve
+                LMISYS = getlmis;
+                [tmin,xfeas] = feasp(LMISYS,[0,0,0,0,1]);
+
+                assert(tmin <= 0,"Observer %d is not observable. tmin = %2.4f\n",i,tmin);
+
+                LSet(:,:,i) = dec2mat(LMISYS,xfeas,L);
+                KSet(:,:,i) = dec2mat(LMISYS,xfeas,K);
+
+                if diagnosticCheck
+                    Rs  = dec2mat(LMISYS,xfeas,Rs);
+                    assert(any(eig(Rs)>0),"Observer %d, Rs eigenvalues not larger than zero",i)
+                    Nu  = dec2mat(LMISYS,xfeas,Nu);
+                    assert(Nu>=0,"Observer %d, Nu (%.2f) is not larger than zero",i,Nu)
+                    Ds  = dec2mat(LMISYS,xfeas,Ds);
+                    assert(any(eig(Ds)>0),"Observer %d, Ds is not positive definite",i)
+                    Mua = dec2mat(LMISYS,xfeas,Mua);
+                    assert(Mua>=0,"Observer %d, Mu_a (%.2f) is not larger than zero",i,Mua)
+                    Mud = dec2mat(LMISYS,xfeas,Mud);
+                    assert(Mud>=0,"Observer %d, Mu_d (%2.f) is not larger than zero",i,Mud)
+                end
             end
         end
     end
